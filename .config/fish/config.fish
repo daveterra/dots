fbbbd07a4ba57fad14ff610c9c56b769ebfd7531
Dave Terra <11788035+daveterra@users.noreply.github.com>
2023-05-29 13:20:04 -0700

    Updates to fish to remove brew and cargo

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,14 +5,6 @@
 # Reference is: https://fishshell.com/docs/current/language.html
 #
 
-# Set up homebrew
-# set -gx HOMEBREW_PREFIX "/opt/homebrew";
-# set -gx HOMEBREW_CELLAR "/opt/homebrew/Cellar";
-# set -gx HOMEBREW_REPOSITORY "/opt/homebrew";
-# set -q PATH; or set PATH ''; set -gx PATH "/opt/homebrew/bin" "/opt/homebrew/sbin" $PATH;
-# set -q MANPATH; or set MANPATH ''; set -gx MANPATH "/opt/homebrew/share/man" $MANPATH;
-# set -q INFOPATH; or set INFOPATH ''; set -gx INFOPATH "/opt/homebrew/share/info" $INFOPATH;
-#
 set -gx LC_ALL en_US.UTF-8
 set -gx LANG en_US.UTF-8
 set -gx LANGUAGE en_US.UTF-8
@@ -20,11 +12,8 @@ set -gx LOCALE_ARCHIVE /usr/lib/locale/locale-archive
 set -gx SHELL (which fish)
 
 # Add to path
-fish_add_path "$HOME/.cargo/bin"
-fish_add_path /usr/local/sbin
-fish_add_path ~/.local/bin
-fish_add_path ~/.cargo/bin
-fish_add_path  /nix/var/nix/profiles/default/bin
+# fish_add_path "$HOME/.cargo/bin"
+# fish_add_path  /nix/var/nix/profiles/default/bin
 
 # Safer (but more annoying?) rm
 function recycling -d "My rm replacement"
@@ -40,7 +29,6 @@ end
 
 function setDarkMode --argument darkmode
   for file in (/bin/ls /tmp/mykitty*)
-
     alias kitty /Applications/kitty.app/Contents/MacOS/kitty
     if $darkmode
       echo "Darkmode is on: $file" > /tmp/hello.txt
@@ -110,7 +98,7 @@ if status is-interactive
 
   function ls --wraps ls -d "ls with preferred options"
     # command ls --group-directories-first --color=always -F1 "$argv"
-    command ls --color=always -F $argv
+    command ls -a --color=always -F $argv
   end
 
   function cd -d "cd and then ls"
@@ -137,14 +125,9 @@ if status is-interactive
       curl cheat.sh/$argv
   end
 
-  # function config --wraps git --description 'An alias for working on dotfiles with git'
-    # command git --git-dir=$XDG_DATA_HOME/dots --work-tree=$HOME $argv
-  # end
-
   # Alias
   alias vi nvim
   alias vim nvim
-  alias mux tmuxinator
   alias ll "ls -alh"
   alias gap "git add --patch -i"
   alias btc "ssh cointop.sh"
@@ -159,17 +142,15 @@ if status is-interactive
   alias router "ssh admin@10.173.0.1"
   alias myip "curl ipinfo.io/ip"
 
-  # https://blog.benoitj.ca/2020-10-03-chezmoi-merging/
-  alias cmm="chezmoi diff --use-builtin-diff | grep 'diff --git' | cut -f3 -d ' ' | sed 's/a\//~\//' | xargs -r chezmoi merge"
-  abbr rm recycling
   alias config "git --git-dir=$XDG_DATA_HOME/dots --work-tree=$HOME $argv"
   set -U fish_key_bindings fish_vi_key_bindings
 
-  function setcursors
-     set -g fish_cursor_default block
-     set -g fish_cursor_insert block
-     set -g fish_cursor_visual block
-  end
+  abbr rm recycling
+  abbr mux tmuxinator
+
+  set -g fish_cursor_default underscore
+  set -g fish_cursor_insert line
+  set -g fish_cursor_visual block
 
   function fish_command_not_found
     if test "$argv[1]" = "fisher"
