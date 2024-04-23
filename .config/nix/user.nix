8811c905d3ab307e2ce5e6cf32504c251df7743a
Dave Terra <11788035+daveterra@users.noreply.github.com>
2024-04-23 07:25:59 -0700

    Sorting and trimming nix config

--- .config/nix/user.nix
+++ .config/nix/user.nix
@@ -1,43 +1,44 @@
 with import <nixpkgs> {}; [
   # Command line tools 
-  fish
-  bash # Needed for tmux/extracto, also just nice to have newer version
-  zoxide # replaces cd 
-  eza # replaces ls 
-  silver-searcher # ag 
-  ncdu # Better du -csh
-  walk # Util for browsing folders
-  entr # Do something when files change
-  restic # Backup util
-  jq # Json tool
-  btop # Fancier top with disk and net usage
-  tree
-  wget
-  pv
-  tmux
-  fzf
-  starship
-  git
-  ripgrep
   aspell
-  ltex-ls
   aspellDicts.en
-  lazygit
-  bitwarden-cli
-  docker-compose
-  nix
-  rsync
-  tmuxinator
-  fswatch
-  nmap
-  python310
-  inetutils
-  flavours
   base16-builder
+  bash # Needed for tmux/extracto, also just nice to have newer version
+  bat # replaces cat
+  bitwarden-cli
+  btop # Fancier top with disk and net usage
   direnv
+  direnv
+  docker-compose
+  entr # Do something when files change
+  eza # replaces ls 
+  fish
+  flavours
+  frogmouth # Markdown reader
+  fswatch
+  fzf
+  git
   git-crypt
-  walk
-  direnv
+  inetutils
+  jq # Json tool
+  lazygit
+  ltex-ls
+  ncdu # Better du -csh
+  nix
+  nmap
+  pv
+  python310
+  restic # Backup util
+  ripgrep
+  rsync
+  silver-searcher # ag 
+  starship
+  tmux
+  tmuxinator
+  tree
+  walk # Util for browsing folders
+  wget
+  zoxide # replaces cd 
 
   # ddgr # duck duck go from terminal
   # mosh # an ssh wrapper
@@ -59,19 +60,17 @@ with import <nixpkgs> {}; [
   # chafa # image viewer
 
   # Helix and LSPs
-  python310Packages.python-lsp-server
-  nodePackages_latest.vscode-langservers-extracted
+  helix
+  marksman
   nodePackages_latest.bash-language-server
+  nodePackages_latest.vscode-langservers-extracted
+  python310Packages.python-lsp-server
+  rnix-lsp
   swift-format
   typos
-  rnix-lsp
-  marksman
-  helix
 
   # Other dev, though this should all be local...
   nodejs
   yarn
   arduino-cli
-  
-  # fun
 ]
