67e12848536c4c51361a0f5d0fcb280b0a32ce58
Dave Terra <11788035+daveterra@users.noreply.github.com>
2024-09-28 07:47:05 -0700

    y

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -89,6 +89,15 @@ if status is-interactive
         set loc (walk --icons $argv); and cd $loc
     end
 
+    function y
+        set tmp (mktemp -t "yazi-cwd.XXXXXX")
+        yazi $argv --cwd-file="$tmp"
+        if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+            builtin cd -- "$cwd"
+        end
+        rm -f -- "$tmp"
+    end
+
     # bind \t complete
     # bind \t forward-word
     # bind \t complete-no-pager
